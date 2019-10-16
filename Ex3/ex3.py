74dcb49d1082d859b22a91c6e74ca799eca43453
John Oatis <joatis@joatis3.com>
2019-10-15 22:04:17 -0400

    created and commented ex3.py

---
+++ Ex3/ex3.py
@@ -0,0 +1,41 @@
+# Print a string
+print("I will now count my chickens:")
+
+# print a string and the result of 30 / 6 + 25 
+print("Hens", 25 + 30 / 6)
+
+# print a string and the result of 100 - (3 % 4) * 25  
+print("Roosters", 100 - 25 * 3 % 4)
+
+# print a string
+print("Now I will count the eggs:")
+
+# print result of the operation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# print a string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# print the result
+print(3 + 2 < 5 - 7)
+
+# print the string and the result.
+print("What is 3 + 2?", 3 + 2)
+
+# print the string and result
+print("What is 5 - 7?", 5 - 7)
+
+# print the string
+print("Oh, thats why it's False.")
+
+# print the string
+print("How about some more.")
+
+# print the string and the boolean result
+print("Is it greater?", 5 >= -2)
+
+# print the string and the boolean result
+print("Is it greater or equal?", 5 >= -2)
+
+# print the string and the boolean result
+print("Is it less or equal?", 5 <= -2)
