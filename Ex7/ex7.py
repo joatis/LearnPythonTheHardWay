d0f560eb47f626faa206fc2b16755987186323b2
John Oatis <joatis@joatis3.com>
2019-10-21 21:01:04 -0400

    Sudy drill for ex7

--- Ex7/ex7.py
+++ Ex7/ex7.py
@@ -1,8 +1,13 @@
+# Print a string
 print("Mary had a little lamb.")
+# Print a string after calling format
 print("Its fleece was white as {}.".format('snow'))
+# Print a string
 print("And everywhere Mary went.")
+# Print a string of 10 '*'
 print("." * 10)  # what'd that do?
 
+# set 12 strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +22,7 @@ end11 = "e"
 end12 = "r"
 
 # watch end = ' ' at the end. try removing it to see what happens
+# Print the string of 12 vars concatenated. 
+# If I remove the end = ' ' it prints 2 lines instead of 1
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
