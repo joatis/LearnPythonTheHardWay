7e58a48f79ce76a0507cf36017c5ebf351888deb
John Oatis <joatis@joatis3.com>
2019-10-21 21:15:36 -0400

    Ex8 study drill

--- EX8/ex8.py
+++ EX8/ex8.py
@@ -1,9 +1,15 @@
+# Initialize a string called formatter
 formatter = "{} {} {} {}"
 
+# call the format function on formatter passing 4 integers
 print(formatter.format(1, 2, 3, 4))
+# call the format function on formatter passing 4 strings
 print(formatter.format("one", "two", "three", "four"))
+# call the format function on formatter passing 4 booleans
 print(formatter.format(True, False, False, True))
+# call the format function on formatter passing 4 instances of the formatter string
 print(formatter.format(formatter, formatter, formatter, formatter))
+# call the format function on formatter passing 4 strings
 print(formatter.format(
   "Try your",
   "Own text here",
